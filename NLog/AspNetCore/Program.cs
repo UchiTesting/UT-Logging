4489e30deb24fbbb7ca8b62006c8c6f5259d7f37
UchiTesing <uchitesting57@gmail.com>
2023-06-27 21:38:55 +0200

    Surround original Program.cs into try catch finally block

--- NLog/AspNetCore/Program.cs
+++ NLog/AspNetCore/Program.cs
@@ -1,27 +1,40 @@
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+try
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+	var builder = WebApplication.CreateBuilder(args);
+
+	// Add services to the container.
+	builder.Services.AddControllersWithViews();
+
+	var app = builder.Build();
+
+	// Configure the HTTP request pipeline.
+	if (!app.Environment.IsDevelopment())
+	{
+		app.UseExceptionHandler("/Home/Error");
+		// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+		app.UseHsts();
+	}
+
+	app.UseHttpsRedirection();
+	app.UseStaticFiles();
+
+	app.UseRouting();
+
+	app.UseAuthorization();
+
+	app.MapControllerRoute(
+		name: "default",
+		pattern: "{controller=Home}/{action=Index}/{id?}");
+
+	app.Run();
+
 }
+catch (Exception e)
+{
 
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
+	throw;
+}
+finally
+{
+	// Will shutdown NLog Manager later on
+}
