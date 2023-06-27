21a73c5f03233d3a56d4696525461b6b358d421a
UchiTesing <uchitesting57@gmail.com>
2023-06-27 21:42:07 +0200

    Update Program.cs to use NLog

--- NLog/AspNetCore/Program.cs
+++ NLog/AspNetCore/Program.cs
@@ -1,3 +1,11 @@
+using NLog;
+using NLog.Web;
+
+// Early init of NLog to allow startup
+// and exception logging, before host is built
+var logger = NLog.LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
+logger.Debug("init main");
+
 try
 {
 	var builder = WebApplication.CreateBuilder(args);
@@ -5,13 +13,19 @@ try
 	// Add services to the container.
 	builder.Services.AddControllersWithViews();
 
+	// NLog: Setup NLog for Dependency injection
+	builder.Logging.ClearProviders();
+	builder.Host.UseNLog();
+
 	var app = builder.Build();
 
 	// Configure the HTTP request pipeline.
 	if (!app.Environment.IsDevelopment())
 	{
 		app.UseExceptionHandler("/Home/Error");
-		// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+		// The default HSTS value is 30 days.
+		// You may want to change this for production scenarios,
+		// see https://aka.ms/aspnetcore-hsts.
 		app.UseHsts();
 	}
 
@@ -31,10 +45,14 @@ try
 }
 catch (Exception e)
 {
-
+	// NLog: catch setup errors
+	logger.Error(e, "Stopped program because of exception");
 	throw;
 }
 finally
 {
-	// Will shutdown NLog Manager later on
+	// Ensure to flush and stop internal timers/threads
+	// before application-exit
+	// (Avoid segmentation fault on Linux)
+	NLog.LogManager.Shutdown();
 }
